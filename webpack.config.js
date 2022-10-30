73c5c549d5dc1e0b8fbd86666d4b346cfe211a90
Steve Higham (MSI Ubuntu) <steve@sjlt.co.uk>
2022-10-30 14:58:47 +0000

    End of 'Composing Configuration' chapter

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,36 @@
-const { mode } = require ("webpack-nano/argv");
-const { MiniHtmlWebpackPlugin } = require ("mini-html-webpack-plugin");
-const { WebpackPluginServe } = require ("webpack-plugin-serve");
+const { mode } = require ('webpack-nano/argv');
+const { merge } = require ('webpack-merge');
+const parts = require ('./webpack.parts');
 
-module.exports = {
+const commonConfig = merge([
+    { entry: ["./src"] },
+    parts.page({ title: "Demo" }),
+]);
+
+const productionConfig = merge([]);
+
+const developmentConfig = merge([
+    { entry: ['webpack-plugin-serve/client'] },
+    parts.devServer()
+]);
+
+const getConfig = (mode) => {
+    process.env.NODE_ENV = mode;
+    switch (mode) {
+        case "production":
+            return merge (commonConfig, productionConfig, { mode });
+        case "development":
+            return merge (commonConfig, developmentConfig, { mode });
+         default:
+            throw new Error (`Trying to use an unknown mode, ${mode}`);
+    }
+};
+
+//const { MiniHtmlWebpackPlugin } = require ("mini-html-webpack-plugin");
+//const { WebpackPluginServe } = require ("webpack-plugin-serve");
+
+module.exports = getConfig (mode);
+    /*{
     watch: mode == 'development',
     entry: ['./src', 'webpack-plugin-serve/client'],
     mode,
@@ -16,5 +44,6 @@ module.exports = {
         })
     ],
 };
+     */
 
 // End of webpack.config.js
